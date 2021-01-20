0f87ed48ac0f130468d28c8bc02f4835479bfd52
Aleulo <69992770+Aleulo@users.noreply.github.com>
2021-01-20 00:52:17 -0800

    Update main.py
    Additional comments

--- main.py
+++ main.py
@@ -40,21 +40,22 @@ def input_valid():
 
 def deal():
     # this function calculates the deal
-    quantity = len(amount_list) - len(cases)
+    quantity = len(amount_list) - len(cases) # quantity of all unpicked numbers equals: length of the list - length of the dict
     result = (all_sum - sum(cases.values())) / quantity
-    print(str(result) + " = (" + str(all_sum) + " - " + str(sum(cases.values())) + ") / " + str(quantity))
+    print(str(result) + " = (" + str(all_sum) + " - " + str(sum(cases.values())) + ") / " + str(quantity))  # helpful output
     return "$"+"{:.2f}".format(result)
 
 
 def interface():
     # the user enters 1 to 10 so we need index
     index = input_valid()  # we call validation of input function
-    number = amount_list[index]
-    cases[index] = number
+    number = amount_list[index]  # the number is the number at specific index from list. This is validated iser decision
+    cases[index] = number  # add this number and index to the dictionary
     return number
 
 
 def ask_ok(prompt, retries=4, reminder="Please try again"):
+    #  standart user asking function. No explanation needed. I took it from the official python tutorial
     while True:
         ok = input(prompt)
         if ok in ('y','ye','yes'):
@@ -67,26 +68,28 @@ def ask_ok(prompt, retries=4, reminder="Please try again"):
         print(reminder)
 
 
-def final_output(out, value):
-    if out:
-        print("User could won ", value)
+def final_output(out, value):  
+    # this function prints final output 
+    if out: # if the user accepted the deal
+        print("User could won ", value) # amout that could be won if the user accepted the deal
         print("The User won", deal())
     else:
         print("The User won", value)
 
 
 def game():
+    # this function is the game mechanic
     print("this is sorted list", amount_list)
-    flag = False
+    flag = False  # this flag shows that the user did not choose deal
     print()
-    for i in range(1, 10):
+    for i in range(1, 10):  # it makes 10 moves
         print("counter = ", i)
-        case_value = interface()
+        case_value = interface()  # this statement calls interface to ask the user for input and put it in the dictionary of the used numbers
         print("this is the new case dictionary after add", cases)
-        if i == 3 or i == 5 or i == 7 or i == 9:
+        if i == 3 or i == 5 or i == 7 or i == 9:  # every 3,5,7,9 move it asks the user for a deal wich is calculated with deal function
             print("The deal is: ", deal())
-            if ask_ok("Do you want to accept the deal?"):
-                flag = True
+            if ask_ok("Do you want to accept the deal?"):  # if ask_ok function returns True then we break and finist the game
+                flag = True  # since the user chose deal, we change to true
                 break
     final_output(flag, case_value)
 
